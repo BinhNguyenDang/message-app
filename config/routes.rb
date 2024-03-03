15c4116aa63650acce622332e066f4256be8f497
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-03-03 13:31:53 -0500

    Setup authentication system and enable flash messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   root 'chatroom#index'
   get 'login', to: 'sessions#new'
-
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
   
   get "up" => "rails/health#show", as: :rails_health_check
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,9 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+User.create(username: "Binh1", password: "123456")
+User.create(username: "Binh2", password: "123456")
+User.create(username: "Binh3", password: "123456")
+User.create(username: "Binh4", password: "123456")
+User.create(username: "Binh5", password: "123456")
