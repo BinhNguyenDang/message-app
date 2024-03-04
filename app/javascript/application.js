3c7504d21feda938b0885376eee45b2991a58072
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-03-04 03:16:29 -0500

    real-time message function

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -4,11 +4,22 @@ import "controllers"
 import * as jquery from "jquery"
 import "semantic-ui"
 
+
+window.scroll_bottom = function(){
+  if ($('#messages').length > 0){
+    $('#messages').scrollTop($('#messages')[0].scrollHeight);
+  }
+}
+
+
+
 $(document).on('turbo:load', function() {
     console.log('loaded turbo links')
     $('.ui.dropdown').dropdown()
     $('.message .close').on('click', function() {
     $(this).closest('.message').transition('fade');
   });
+  scroll_bottom();
 });
 
+import "channels"
