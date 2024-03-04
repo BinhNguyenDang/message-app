1a871f9d037d8785ab2a01bda4b26977c12f703d
Binh Nguyen Dang <binhnguyendang2003@gmail.com>
2024-03-04 00:43:27 -0500

    Add message posting functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  get 'messages/create'
   root 'chatroom#index'
+  
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  post 'message', to: 'messages#create'
   
   get "up" => "rails/health#show", as: :rails_health_check
 
